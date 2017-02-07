e91961089056f283f620b4d4f08aa155495c1875
davellaj <jamie.davella@gmail.com>
2017-02-07 16:18:06 -0500

    implement google strategy and bearer strategy on get / route on server

--- models/user.js
+++ models/user.js
@@ -1,1 +1,14 @@
 const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  googleId: String,
+  name: Object,
+  accessToken: String,
+  userName: { type: String, required: true },
+  email: Object,
+  picture: String
+});
+
+const User = mongoose.model('user', userSchema);
+module.exports = User;
