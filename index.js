aa1d33034a100c5d1fb3670c0155bf51b292c0df
numbergames <bfletch@gmail.com>
2017-02-08 16:23:06 -0500

    begin work on photos schema

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(jsonParser);
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    next();
+  next();
 });
 
 mongoose.Promise = global.Promise;
