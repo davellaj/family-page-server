44c8a358bc72cee81e2e60bd94b1277613f11e85
Ben Fletcher <numbergames@users.noreply.github.com>
2017-02-13 17:41:17 -0500

    Initial deployment fixes (#10)
    * remove config require
    
    * remove auth code
    
    * Fix environment variables

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { DATABASE_URL, PORT, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = require('./config');
-const GoogleStrategy = require('passport-google-oauth20');
-const passport = require('passport');
-const BearerStrategy = require('passport-http-bearer');
+// const { DATABASE_URL, PORT, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = require('./config');
+// const GoogleStrategy = require('passport-google-oauth20');
+// const passport = require('passport');
+// const BearerStrategy = require('passport-http-bearer');
 
-const User = require('./models/user');
+// const User = require('./models/user');
 const Photos = require('./models/photos');
 const Members = require('./models/members');
 
@@ -28,58 +28,58 @@ app.all('/*', (req, res, next) => {
 mongoose.Promise = global.Promise;
 
 // google auth
-passport.use(new GoogleStrategy(
-  {
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
-  },
-  (accessToken, refreshToken, profile, cb) => {
-    console.log('this is the profile photo', profile.photos[0].value);
-    User.findOneAndUpdate({ googleId: profile.id }, {
-      $set: {
-        googleId: profile.id,
-        name: profile.name,
-        userName: profile.displayName,
-        email: profile.emails[0].value,
-        picture: profile.photos[0].value,
-        accessToken
-      }
-    },
-    { upsert: true, new: true, setDefaultsOnInsert: true })
-    .then((user) => {
-      console.log(user);
-      cb(null, user);
-    })
-    .catch((err) => {
-      console.log('catch error', err);
-    });
-  })
-);
+// passport.use(new GoogleStrategy(
+//   {
+//     clientID: GOOGLE_CLIENT_ID,
+//     clientSecret: GOOGLE_CLIENT_SECRET,
+//     callbackURL: '/auth/google/callback'
+//   },
+//   (accessToken, refreshToken, profile, cb) => {
+//     console.log('this is the profile photo', profile.photos[0].value);
+//     User.findOneAndUpdate({ googleId: profile.id }, {
+//       $set: {
+//         googleId: profile.id,
+//         name: profile.name,
+//         userName: profile.displayName,
+//         email: profile.emails[0].value,
+//         picture: profile.photos[0].value,
+//         accessToken
+//       }
+//     },
+//     { upsert: true, new: true, setDefaultsOnInsert: true })
+//     .then((user) => {
+//       console.log(user);
+//       cb(null, user);
+//     })
+//     .catch((err) => {
+//       console.log('catch error', err);
+//     });
+//   })
+// );
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] }));
+// app.get('/auth/google',
+//   passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/', session: false }),
-  (req, res) => {
-    res.cookie('accessToken', req.user.accessToken, { expires: 0, httpOnly: false });
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
+// app.get('/auth/google/callback',
+//   passport.authenticate('google', { failureRedirect: '/', session: false }),
+//   (req, res) => {
+//     res.cookie('accessToken', req.user.accessToken, { expires: 0, httpOnly: false });
+//     // Successful authentication, redirect home.
+//     res.redirect('/');
+//   });
 
 // passport bearer Strategy
-passport.use(new BearerStrategy(
-  (accessToken, done) => {
-    User.findOne({
-      accessToken
-    }).then((user) => {
-      done(null, user, { scope: 'read' });
-    }).catch((err) => {
-      done(err, null);
-    });
-  }
-));
+// passport.use(new BearerStrategy(
+//   (accessToken, done) => {
+//     User.findOne({
+//       accessToken
+//     }).then((user) => {
+//       done(null, user, { scope: 'read' });
+//     }).catch((err) => {
+//       done(err, null);
+//     });
+//   }
+// ));
 
 // get users homepage with authenticated route
 // app.get('/', passport.authenticate('bearer', { session: false }),
@@ -151,15 +151,15 @@ let server;
 
 function runServer(callback) {
   return new Promise((resolve, reject) => {
-    mongoose.connect(DATABASE_URL, (err) => {
+    mongoose.connect(process.env.DATABASE_URL, (err) => {
       if (err && callback) {
         console.log(err);
         return callback(err);
       }
       return null;
     });
-    server = app.listen(PORT, HOST, () => {
-      console.log(`Your app is listening on port ${PORT}`);
+    server = app.listen(process.env.PORT, HOST, () => {
+      console.log(`Your app is listening on port ${process.env.PORT}`);
       if (callback) {
         callback();
       }
