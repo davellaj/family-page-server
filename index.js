9c1a0e319c5e3496da9ae45cf5e1c2d5e30e2bb4
Ben Fletcher <numbergames@users.noreply.github.com>
2017-02-14 18:03:16 -0500

    Cleanup code, prepare for endpoint work (#12)
    * implement environment variables, dotenv
    
    * create Comments schema and update readme
    
    * Begin work on comments endpoint. Hardcoded message saved to message.
    
    * Cleanup unused code

--- index.js
+++ index.js
@@ -3,92 +3,25 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
-dotenv.config();
-// const { DATABASE_URL, PORT, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = require('./config');
-// const GoogleStrategy = require('passport-google-oauth20');
-// const passport = require('passport');
-// const BearerStrategy = require('passport-http-bearer');
-
-// const User = require('./models/user');
 const Messages = require('./models/messages');
 const Members = require('./models/members');
 
 const HOST = process.env.HOST;
 
+dotenv.config();
+
+mongoose.Promise = global.Promise;
+
 console.log(`Server running in ${process.env.NODE_ENV} mode`);
 
 const app = express();
-const jsonParser = bodyParser.json();
-
-app.use(jsonParser);
+app.use(bodyParser.json());
 
 app.all('/*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
   next();
 });
-
-mongoose.Promise = global.Promise;
-
-// google auth
-// passport.use(new GoogleStrategy(
-//   {
-//     clientID: GOOGLE_CLIENT_ID,
-//     clientSecret: GOOGLE_CLIENT_SECRET,
-//     callbackURL: '/auth/google/callback'
-//   },
-//   (accessToken, refreshToken, profile, cb) => {
-//     console.log('this is the profile photo', profile.photos[0].value);
-//     User.findOneAndUpdate({ googleId: profile.id }, {
-//       $set: {
-//         googleId: profile.id,
-//         name: profile.name,
-//         userName: profile.displayName,
-//         email: profile.emails[0].value,
-//         picture: profile.photos[0].value,
-//         accessToken
-//       }
-//     },
-//     { upsert: true, new: true, setDefaultsOnInsert: true })
-//     .then((user) => {
-//       console.log(user);
-//       cb(null, user);
-//     })
-//     .catch((err) => {
-//       console.log('catch error', err);
-//     });
-//   })
-// );
-
-// app.get('/auth/google',
-//   passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-// app.get('/auth/google/callback',
-//   passport.authenticate('google', { failureRedirect: '/', session: false }),
-//   (req, res) => {
-//     res.cookie('accessToken', req.user.accessToken, { expires: 0, httpOnly: false });
-//     // Successful authentication, redirect home.
-//     res.redirect('/');
-//   });
-
-// passport bearer Strategy
-// passport.use(new BearerStrategy(
-//   (accessToken, done) => {
-//     User.findOne({
-//       accessToken
-//     }).then((user) => {
-//       done(null, user, { scope: 'read' });
-//     }).catch((err) => {
-//       done(err, null);
-//     });
-//   }
-// ));
-
-// get users homepage with authenticated route
-// app.get('/', passport.authenticate('bearer', { session: false }),
-//   (req, res) => {
-//     res.status(200).json({message: 'Hello from the server this route was protected'})
-// })
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello from the server.' });
@@ -146,6 +79,25 @@ app.get('/members', (req, res) => {
 app.post('/members', ({ body }, res) => {
   Members.create(body)
   .then(data => res.status(200).json(data));
+});
+
+// ===== COMMENTS =====
+
+app.post('/comments/:userId/:messageId', (req, res) => {
+  console.log('here');
+  const testMessage = {
+    from: 'Casey',
+    to: 'Alex',
+    text: 'I solved it on the first try.'
+  };
+  Messages.findById(req.params.messageId)
+  .then((message) => {
+    message.comments.push(testMessage);
+    message.save();
+    return message;
+  })
+  .then(message => message.save())
+  .then(data => res.json(data).sendStatus(200));
 });
 
 // ===== SERVER =====
--- models/messages.js
+++ models/messages.js
@@ -1,13 +1,5 @@
 const mongoose = require('mongoose');
 
-const Comments = new mongoose.Schema({
-  from: { type: String, required: true },
-  to: { type: String, required: true },
-  comment: { type: String, required: true },
-  date: { type: Date, default: Date.now }
-});
-
-
 const messages = new mongoose.Schema({
   contentType: { type: String, required: true },
   url: { type: String, required: true },
@@ -15,7 +7,12 @@ const messages = new mongoose.Schema({
   date: { type: Date, default: Date.now },
   userId: { type: String, required: true },
   tags: Array,
-  comments: [Comments]
+  comments: [{
+    from: { type: String, required: true },
+    to: { type: String, required: true },
+    text: { type: String, required: true },
+    date: { type: Date, default: Date.now }
+  }]
 });
 
 const Messages = mongoose.model('photos', messages);
