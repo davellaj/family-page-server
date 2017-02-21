dfea3de018d41534cf55e2256ead813a7eee6036
numbergames <bfletch@gmail.com>
2017-02-21 15:00:53 -0500

    specify url

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.all('/*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', frontendUrl);
   res.header('Access-Control-Allow-Headers',
     'Content-Type, '
     + 'Access-Control-Allow-Headers, '
